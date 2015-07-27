64b161344212dbb2e9eeb8a6e99ba7fefe92c22f
onur anli <onur.anli36@gmail.com>
2015-07-27 16:34:58 +0300

    Create Degiskenler.java

---
+++ Degiskenler.java
@@ -0,0 +1,32 @@
+public class Degiskenler {
+	
+
+	public static void main(String[] args) {
+		int a = 5;
+		char c= 'c';/*tek karakter*/
+		float d = 5;
+		String isim;
+		isim="onur";
+	    final double sabit = 3.14;//  final ile sabitleştirme yapılıyor.Class içinde değişmek mümkün değil.
+	   
+		
+		
+		System.out.println(isim);
+		System.out.println(a);
+		System.out.println(c);
+		System.out.println(d+"\n"+"hello world");//\n bir alt satıra alır
+		System.out.println(sabit);
+		
+		
+	
+	}
+	
+}
+/*          ÇIKTI         
+onur
+5
+c
+5.0
+hello world
+3.14
+*/
